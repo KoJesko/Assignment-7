81db9e449929daabbeb2c4cb05f92b9e9da8acd2
Ari Kotler <jansherremway@gmail.com>
2024-07-09 18:06:07 -0400

    Main 1.0

---
+++ Assignment 7.py
@@ -0,0 +1,27 @@
+# Using a for loop
+print("Using for loop:")
+for i in range(26):
+    print(i)
+
+# Using a while loop
+print("\nUsing while loop:")
+i = 0
+while i < 26:
+    print(i)
+    i += 1
+    
+# Bonus
+
+# Using a for loop
+print("Using for loop:")
+for i in range(1, 26):
+    if i % 7 == 0:
+        print(i)
+
+# Using a while loop
+print("\nUsing while loop:")
+i = 1
+while i < 26:
+    if i % 7 == 0:
+        print(i)
+    i += 1
